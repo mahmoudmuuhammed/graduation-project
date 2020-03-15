8f59a36151ea3560a26a200d08d35dcdd9d12b91
MahmoudFCI <51200278+FCI2020@users.noreply.github.com>
2020-03-16 00:56:27 +0200

    Upload signup form
    Style and validation of sign up form without backend

--- src/app/land-part/auth/signup-page/signup.component.ts
+++ src/app/land-part/auth/signup-page/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'signup-page',
@@ -6,4 +7,53 @@ import { Component } from "@angular/core";
     styleUrls: ['./signup.component.scss']
 })
 
-export class SignupPageComponent {}
+export class SignupPageComponent implements OnInit {
+    // form group object
+    signupForm: FormGroup;
+        // validation errors
+    validationMessageError = {
+        required: 'required',
+        notValidEmail: 'notValidEmail',
+        matching: 'two password not matching'
+    };
+    
+    ngOnInit() {
+        this.signupFormController();
+    }
+
+    signupFormSubmit() {
+        
+    }
+
+        // Form controller 
+    private signupFormController() {
+        this.signupForm = new FormGroup({
+            'firstname': new FormControl(null, Validators.required),
+            'lastname': new FormControl(null, Validators.required),
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, Validators.required),
+            'cpassword': new FormControl(null, [Validators.required])
+        });
+    }
+
+        // shortcuts for forms validation 
+    get fnameControl() {
+        return this.signupForm.get('firstname');
+    }
+
+    get lnameControl() {
+        return this.signupForm.get('lastname');
+    }
+
+    get emailControl() {
+        return this.signupForm.get('email');
+    }
+
+    get passControl() {
+        return this.signupForm.get('password');
+    }
+
+    get cpassControl() {
+        return this.signupForm.get('cpassword');
+    }
+}
