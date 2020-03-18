58af50979cba52966a6921de1558833d307db285
MahmoudFCI <51200278+FCI2020@users.noreply.github.com>
2020-03-18 18:56:55 +0200

    update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCPBUsCCVJ5j_AAHmKyMHLAc0Tgu7gQaQc",
-    authDomain: "medkitc.firebaseapp.com",
-    databaseURL: "https://medkitc.firebaseio.com",
-    projectId: "medkitc",
-    storageBucket: "medkitc.appspot.com",
-    messagingSenderId: "155055518569",
-    appId: "1:155055518569:web:b607e06a4bb3276a7203f8",
-    measurementId: "G-QFC77J5M9S"
-  }
+  
 };
 
 /*
