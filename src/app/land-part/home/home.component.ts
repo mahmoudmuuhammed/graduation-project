f9cf52d8771c3b336cc744a39d9b25380a0a8f4e
Moemen Said <MoemenSaid@github.com>
2020-03-26 08:06:13 +0200

    fixing home

--- src/app/land-part/home/home.component.ts
+++ src/app/land-part/home/home.component.ts
@@ -8,24 +8,6 @@ import { Component } from "@angular/core";
 })
 
 export class HomePageComponent {
-
-
-
-    constructor(@Inject(DOCUMENT) private document: Document) { }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if (document.body.scrollTop > 70 ||  document.documentElement.scrollTop > 70) {
-      document.getElementById('navContainer').classList.add('white');
-      (document.getElementById('logo') as HTMLImageElement).src = '../assets/images/logo.svg';
-      (document.getElementById('miniLogo') as HTMLImageElement).src = '../assets/images/miniLogo.svg';
-    }
-    else{
-        document.getElementById('navContainer').classList.remove('white');
-        (document.getElementById('logo') as HTMLImageElement).src = '../assets/images/logoWhite.svg';
-        (document.getElementById('miniLogo') as HTMLImageElement).src = '../assets/images/miniLogoWhite.svg';
-    }
-  }
   name = 'Angular';
   
 }
